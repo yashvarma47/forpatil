d933ac9c394d3f64180def96ceb8a8edc6838db0
yasho <yasho@maveric-systems.com>
2024-04-09 20:22:37 +0530

    For Eureka Service

---
+++ EuerkaServer/src/main/java/com/thinknxt/rba/eurekaserver/EuerkaServerApplication.java
@@ -0,0 +1,15 @@
+package com.thinknxt.rba.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EuerkaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuerkaServerApplication.class, args);
+	}
+
+}
---
+++ EuerkaServer/src/test/java/com/thinknxt/rba/eurekaserver/EuerkaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinknxt.rba.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuerkaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
